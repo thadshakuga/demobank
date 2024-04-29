b1bcdc6bcb6f418020997280b2c5775632dce64a
Thadsayini Kuganendra <kthadsha58@gmail.com>
2024-04-29 06:50:04 +0000

    Fragment created

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,15 +1,41 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/m/Dialog",
+    "sap/m/Button",
+    "sap/m/library",
+    "sap/m/List",
+    "sap/m/StandardListItem",
+    "sap/m/Text"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, Dialog, Button) {
         "use strict";
 
         return Controller.extend("com.sap.demobank.controller.App", {
             onInit: function () {
 
+            },
+            onOpenBankDetails: function () {
+                //create easily
+                if (!this.moreDetails) {
+
+                    this.moreDetails = this.loadFragment(
+                        { name: "com.sap.demobank.view.fragments.MoreDetails" }
+                    );
+                }
+
+                this.moreDetails.then(
+                    function (oDialog) {
+                        //this.oDialog.Open();
+                        oDialog.open();
+                    }
+                );
+            },
+            onCloseBankDetails:function(){
+                this.byId("moreBankDetails").close();
             }
+
         });
     });
